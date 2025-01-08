0898423cc0affb3f1d0145e75627b4291ed7638e
sophie <scboyer.dev@gmail.com>
2025-01-08 17:28:24 +0100

    update payment route

--- ROMAN/routes/order.js
+++ ROMAN/routes/order.js
@@ -3,44 +3,49 @@ const Order = require("../models/Order");
 const router = express.Router();
 
 // Enregistrer l'username quand une partie est jouée
-router.post("/ROMAN/order", async (req, res) => {
-  try {
-    if (req.body.payed && req.body.email && req.body.details && req.body.ref) {
-      const today = new Date().toLocaleString("fr-FR", {
-        timeZone: "Europe/Paris",
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-      });
-      const newOrder = new Order({
-        ref: req.body.ref,
-        date: today,
-        name: req.body.name,
-        email: req.body.email,
-        dedication: req.body.dedication,
-        nameToDedicate: req.body.nameToDedicate,
-        status: "payée",
-        details: req.body.details,
-      });
-      await newOrder.save();
-      return res.status(200).json({
-        message:
-          "Votre commande a bien été enregistrée. Vous recevrez bientôt un email de Mondial Relay pour choisir votre point relais.",
-      });
-    }
-  } catch (error) {
-    return res.status(400).json({ message: error.message });
-  }
-});
+// router.post("/ROMAN/order", async (req, res) => {
+//   try {
+//     if (req.body.payed && req.body.email && req.body.details && req.body.ref) {
+//       const today = new Date().toLocaleString("fr-FR", {
+//         timeZone: "Europe/Paris",
+//         year: "numeric",
+//         month: "2-digit",
+//         day: "2-digit",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//       });
+//       const newOrder = new Order({
+//         ref: req.body.ref,
+//         date: today,
+//         name: req.body.name,
+//         email: req.body.email,
+//         dedication: req.body.dedication,
+//         nameToDedicate: req.body.nameToDedicate,
+//         status: "payée",
+//         details: req.body.details,
+//       });
+//       await newOrder.save();
+//       return res.status(200).json({
+//         message:
+//           "Votre commande a bien été enregistrée. Vous recevrez bientôt un email de Mondial Relay pour choisir votre point relais.",
+//       });
+//     }
+//   } catch (error) {
+//     return res.status(400).json({ message: error.message });
+//   }
+// });
 
 router.get("/ROMAN/orderRef", async (req, res) => {
   try {
     const orders = await Order.find();
-    const ref = `STRIPE-${orders.length + 1}`;
+    let month = new Date().getUTCMonth() + 1;
+    if (month < 10) {
+      month = `0${month}`;
+    }
+    const year = new Date().getUTCFullYear().toString().slice(-2);
+    const orderRef = `LDH${year}${month}${orders.length + 1}ST`;
     return res.status(200).json({
-      ref,
+      orderRef,
     });
   } catch (error) {
     return res.status(400).json({ message: error.message });
--- ROMAN/routes/payment.js
+++ ROMAN/routes/payment.js
@@ -1,23 +1,60 @@
 const express = require("express");
 const router = express.Router();
 const createStripe = require("stripe");
+const Order = require("../models/Order");
 
 const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
 router.post("/payment", async (req, res) => {
   console.log(req.body);
-  try {
-    let { status } = await stripe.charges.create({
-      amount: (req.body.amount * 100).toFixed(0),
-      currency: "eur",
-      description: `Paiement LDH pour : ${req.body.title}`,
-      source: req.body.stripeToken,
-    });
-    res.json({ status });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
+  if (
+    req.body.payed &&
+    req.body.email &&
+    req.body.orderRef &&
+    req.body.amount &&
+    req.body.stripeToken
+  )
+    try {
+      const newOrder = new Order({
+        ref: req.body.orderRef,
+        date: today,
+        name: req.body.name,
+        email: req.body.email,
+        dedication: req.body.dedication,
+        nameToDedicate: req.body.nameToDedicate,
+        status: "commandée",
+        details: req.body.details,
+      });
+      await newOrder.save();
+      let { status } = await stripe.charges.create({
+        amount: (req.body.amount * 100).toFixed(0),
+        currency: "eur",
+        description: `Paiement de votre commande : ${req.body.orderRef}`,
+        source: req.body.stripeToken,
+      });
+      const today = new Date().toLocaleString("fr-FR", {
+        timeZone: "Europe/Paris",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      await Order.findOneAndUpdate(
+        { orderRef: req.body.orderRef },
+        {
+          status: "payée",
+        },
+        { new: true }
+      );
+      return res.status(200).json({
+        message:
+          "Votre commande a bien été enregistrée. Vous recevrez bientôt un email de Mondial Relay pour choisir votre point relais.",
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
 });
 
 module.exports = router;
